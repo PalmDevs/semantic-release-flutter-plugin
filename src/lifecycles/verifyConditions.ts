cb79521a8fc6a40d3c3c5f5f5c4e03bbc8d6a63c
Palm <palmpasuthorn@gmail.com>
2023-04-24 13:15:51 +0700

    feat(lifecycles/verifyConditions): add more validators

--- src/lifecycles/verifyConditions.ts
+++ src/lifecycles/verifyConditions.ts
@@ -10,17 +10,60 @@ export default function verifyConditions(
     const { logger } = context;
     if (!pluginConfig) throw new AggregateError(['No options passed']);
 
+    logger.log('Verifying options: %s', pluginConfig);
+
     const weightErrors: Error[] = [];
 
     (Object.keys(pluginConfig) as (keyof typeof pluginConfig)[]).forEach(
         key => {
             if (
                 key.endsWith('Weight') &&
-                !['undefined', 'number'].includes(typeof pluginConfig[key])
+                !(
+                    typeof pluginConfig === 'undefined' ||
+                    (typeof pluginConfig[key] === 'number' &&
+                        // @ts-expect-error Shut up
+                        pluginConfig[key] >= 0)
+                )
             )
-                weightErrors.push(new Error(`Option ${key} must be a number`));
+                weightErrors.push(
+                    new Error(
+                        `Option ${key} (${pluginConfig[key]}) must be a number higher than 0`
+                    )
+                );
         }
     );
+
+    if (pluginConfig.minorWeight! >= pluginConfig.majorWeight!)
+        weightErrors.push(
+            new Error(
+                `Option minorWeight must be lower than majorWeight (${pluginConfig.majorWeight})`
+            )
+        );
+    if (pluginConfig.patchWeight! >= pluginConfig.minorWeight!)
+        weightErrors.push(
+            new Error(
+                `Option patchWeight must be lower than minorWeight (${pluginConfig.minorWeight})`
+            )
+        );
+    if (pluginConfig.channelWeight! >= pluginConfig.patchWeight!)
+        weightErrors.push(
+            new Error(
+                `Option channelWeight must be lower than patchWeight (${pluginConfig.patchWeight})`
+            )
+        );
+    if (
+        pluginConfig.preReleaseWeight! >=
+        (pluginConfig.channelWeight || pluginConfig.patchWeight)!
+    )
+        weightErrors.push(
+            new Error(
+                `Option preReleaseWeight must be lower than ${
+                    pluginConfig.channelWeight
+                        ? `channelWeight (${pluginConfig.channelWeight})`
+                        : `patchWeight (${pluginConfig.patchWeight})`
+                }}`
+            )
+        );
 
     if (weightErrors.length > 0) throw new AggregateError(weightErrors);
 
